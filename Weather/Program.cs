66d15347518e5a3d23c3f12b38f881b78026d65b
LSLIM <LSLIM@LS>
2018-07-11 21:58:50 +0200

    I write some codes

--- Weather/Program.cs
+++ Weather/Program.cs
@@ -7,9 +7,49 @@ using System.Threading.Tasks;
 namespace Weather
 {
     class Program
-    {
+    {        
+        /* Instructies
+        Maak een applicatie Weather. De applicatie Weather heeft een class Weather. 
+        De class Weather heeft een instance variabele producten, wat een array is.
+        Het arrayobject, bevat objecten van het type Temperature.
+        De class Temperature heeft de string sun en de int windspeed als attributen. Maak de class Temperature abstract.
+        Maak van Temperature twee subclasses, namelijk Warm en Cold en initialiseert in de subclasses voor ieder attribuut een waarde. 
+        Laat in een demo-applicatie zien dat er Warm en Cold in de Weather geplaatst kunnen worden. */
+
         static void Main(string[] args)
         {
+        }
+    }
+
+    class Weather
+    {
+        List<Temperature> temperature;
+
+        public Weather()
+        {
+            temperature = List<Temperature>();
+        }
+    }
+
+    abstract class Temperature
+    {
+        public string Sun;
+        public int Windspeed;
+    }
+
+    class Warm : Temperature
+    {
+        public void Sunshine(String Sun)
+        {
+            Sun = "VeryHot";
+        }
+    }
+
+    class Cold : Temperature
+    {
+        public void Wind(int Windspeed)
+        {
+            Windspeed = 15;
         }
     }
 }
